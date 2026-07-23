2b5a09804da7fbdf8ec182668dfef6b82f5731f0
Rohit Kumar <r56800414@gmail.com>
2026-07-23 22:18:40 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,212 @@
+
+#str1 = "jrierjiijiejwwk"
+#str2 = 'iqwjqwwijqwidjq'
+#str3 = """fefjewfjwiwwjw"""
+
+
+#"djijjqijqkdqqwwdq'xmxssasm"
+#'mdkkfmkffmkmk" rrkwwwrffwl'
+
+#str1 = "rohit.jjggrjgerj \nqwkkdnqwjdnqwjdn"
+#print(str1)
+
+#str1 = "rohit.jjggrjgerj \tqwkkdnqwjdnqwjdn"
+#print(str1)
+"""
+str1 = "jrierjiijiejwwk"
+str2 = "iqwjqwwijqwidjq"
+print(str1+str2)
+"""
+"""
+str1 = "jrierjiijiejwwk"
+str2 = "iqwjqwwijqwidjq"
+finl_str = str1+str2
+print(finl_str)
+"""
+
+"""
+str1 = "jrierjiijiejwwk"
+print(len(str1))
+
+str2 = "jrierjiijiejwwk"
+len2 = len(str2)
+print(len2)
+
+final_str = str1 + " " + str2
+print(final_str)
+print(len(final_str))
+"""
+"""
+str = "rohit kumar"
+ch = str[2]
+print(ch)
+
+str = "rohit kumsarn"
+print(str[4])
+"""
+"""
+str = "rohit kumsar"
+print(str[1:4])
+print(str[0:12])
+print(str[1:len(str)])
+print(str[3:]) #[3:len(str)]
+print(str[:4]) #[0:4]
+"""
+"""
+str = "apple"
+print(str[-3:-1])
+print(str[-5:-2])
+"""
+"""
+str = "i am studying python from apnacollege"
+print(str.endswith("ege"))
+
+str = "i am studying python from apnacollege"
+print(str.capitalize())
+
+str = "i am studying python from apnacollege"
+str = str.capitalize()
+print(str)
+print(str)
+"""
+"""
+str = "i am studying python from apnacollege"
+print(str.replace("python" , "javascript"))
+
+str = "i am studying python from apnacollege"
+print(str.find("from"))
+
+str = "i am from studying python from apnacollege"
+print(str.count("o"))
+"""
+
+# light = "green"
+
+# if(light == "red"):
+#     print("stop")
+# elif(light == "green"):
+#     print("go")
+# elif(light == "yellow"):
+#     print("look")
+
+# print("end of code")
+
+# num =  5
+
+# if(num >2):
+#     print("greater than 2")
+# if(num >3):
+#     print("greater than 3")
+
+
+# num =  5
+
+# if(num >2):
+#     print("greater than 2")
+# elif(num >3):
+#     print("greater than 3")
+
+
+# light = "pink"
+
+# if(light == "red"):
+#     print("stop")
+# elif(light == "green"):
+#     print("go")
+# elif(light == "yellow"):
+#     print("look")
+# else:
+#     print("light is broken")
+
+
+# print("end of code")
+
+# age = 24
+
+# if(age>=18):
+#     print("can vote")
+# else:
+#     print("can not vote")
+
+
+# marks =74
+
+# if(marks >= 90):
+#   grade = "A"  
+# elif(marks >=70 and marks < 90):
+#   grade = "B"
+# elif(marks >= 70 and marks < 80):
+#   grade = "C"
+# else:
+#   grade = "D"
+
+# print("grade of the student ->", grade)
+
+# marks = int(input("enter student marks : "))
+
+# if(marks >= 90):
+#   grade = "A"  
+# elif(marks >=70 and marks < 90):
+#   grade = "B"
+# elif(marks >= 70 and marks < 80):
+#   grade = "C"
+# else:
+#   grade = "D"
+
+# print("grade of the student ->", grade)
+
+# age = 34
+
+# if(age >= 18):
+#     if(age >=80):
+#         print("cannot drive")
+#     else:
+#         print("can drive")
+# else:
+#     print("cannot drive")
+
+# num = int(input("enter numer: "))
+
+# rem = num % 2
+
+# if(rem == 0):
+#     print("EVEN")
+# else:
+#     print("ODD")
+
+# a = int(input("enter first number: "))
+# b = int(input("enter second number: "))
+# c = int(input("enter third number: "))
+
+# if(a >= b and a >= c):
+#     print("first number is largest", a)
+# elif(b >= c):
+#     print("second number is largest", b)
+# else:
+#     print("third is largest", c)
+
+# a = int(input("enter first number: "))
+# b = int(input("enter second number: "))
+# c = int(input("enter third number: "))
+# d = int(input("enter forth number: "))
+
+
+# if(b >= a and d >= c):
+#     print("first number is largest", a)
+# elif(b >= c):
+#     print("second number is largest", b)
+# elif(b >= c):
+#     print("third number is largest", c)    
+# else:
+#     print("forth is largest", d)
+
+# x = int(input("enter number:" ))
+
+# if(x % 5 == 0):
+#     print("multiple of 5")
+# else:
+#     print("not a multiple")
+
+
+
+
